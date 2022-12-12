62c8d3566ec100e07fa7725243fbab8c6c6a6bee
Helen Lehrer <hlehrer@outlook.com>
2022-12-11 17:28:10 -0800

    Add useReducer

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState, useReducer } from 'react';
+
+const initialState = {
+  counter: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {
+        counter: state.counter + 1
+      };
+    default:
+      throw new Error(`There is no action matching ${action.type}.`);
+  }
+}
+
+function Counter () {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    document.title = state.counter;
+  }, [state.counter]);
+
+  return (
+    <>
+      {hidden ? <h1>Count Hidden</h1> : <h1>{state.counter}</h1>}
+      <button onClick={() => dispatch({type: 'increment'})}>Count!</button>
+      <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
+    </>
+  );
+}
+
+export default Counter;
